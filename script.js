a9c83c342f3c080aa6a9252af4f257a2fa545a53
tanuj-singh407 <114893993+tanuj-singh407@users.noreply.github.com>
2024-01-21 18:17:00 +0530

    all the files are added.

---
+++ script.js
@@ -0,0 +1,11 @@
+function processAddress() {
+
+    document.getElementById('product').style.display = 'none';
+    document.getElementById('address').style.display = 'block';
+}
+
+function processPayment() {
+    
+    document.getElementById('address').style.display = 'none';
+    document.getElementById('payment').style.display = 'block';
+}
